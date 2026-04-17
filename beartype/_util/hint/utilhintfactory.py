d23a4f01f4661ee2227e874e09f822a3488d821f
varunethara-dotcom <varun.ethara@ethara.ai>
2026-04-17 12:23:24 +0000

    Add spec PDF for beartype

--- beartype/_util/hint/utilhintfactory.py
+++ beartype/_util/hint/utilhintfactory.py
@@ -59,8 +59,7 @@ class _TypeHintTypeFactoryMeta(type):
         # "TypeHintTypeFactory" class specific to this arbitrary type.
         return super().__call__(type_factory)
 
-# ....................{ CLASSES                            }....................
-class TypeHintTypeFactory(Generic[T], metaclass=_TypeHintTypeFactoryMeta):
+# ....................{ CLASSES                            }....................class TypeHintTypeFactory(Generic[T], metaclass=_TypeHintTypeFactoryMeta):
     '''
     **Type hint type factory** (i.e., high-level object unconditionally
     returning an arbitrary type when subscripted by any arbitrary object).
